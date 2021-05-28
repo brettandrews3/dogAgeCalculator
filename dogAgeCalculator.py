6b68ce72f051b388db8e12158ce12eac8f5d8009
brettandrews3 <brettandrews3@gmail.com>
2021-05-28 15:34:05 -0500

    1st commit. Setting up program to calculate human age in months or dog age in human years.

--- dogAgeCalculator.py
+++ dogAgeCalculator.py
@@ -1,9 +1,13 @@
 # This program calculates a dog's age by taking input for dog name & age
 # before printing a statement that returns dog name and dog dog age.
 
-print('Alright, we\'re going to calculate your dog\'s age in dog years. Ready?\n')
+choice = input("We're doing age calculations here. Do you want your age in months or your dog's age in human years?\n")
+
+
+
+print("Alright, we\'re going to calculate your dog\'s age in dog years. Let's go!\n")
 # sleep = 1   (How do you write a time delay in a Python program?)
-dogName = input('What is your dog\'s name?\n')
+dogName = input("What is your dog's name?\n")
 dogDogAge = input('Got it. How old is ' + dogName + '?\n')
 
 #dogIntAge = int(dogDogAge)
