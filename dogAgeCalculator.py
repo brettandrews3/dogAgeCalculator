26c158adaff4f54cc57e6fb774cafd603112ce86
brettandrews3 <brettandrews3@gmail.com>
2021-05-28 16:04:14 -0500

    Dog choice works. Need to write elif logic for Human, and also an Exception clause for wrong input.

--- dogAgeCalculator.py
+++ dogAgeCalculator.py
@@ -1,18 +1,18 @@
-# This program calculates a dog's age by taking input for dog name & age
-# before printing a statement that returns dog name and dog dog age.
+# This calculates either a human's age in months or dog's age in human years.
 
-choice = input("We're doing age calculations here. Do you want your age in months or your dog's age in human years?\n")
+print("We're doing age calculations here. Do you want your age in months or your dog's age in human years?\n")
+choice = input("Enter 'human' or 'dog':\n")
+if choice == 'dog':
+    print("Alright, we\'re going to calculate your dog\'s age in dog years. Let's go!\n")
+    # sleep = 1   (How do you write a time delay in a Python program?)
+    dogName = input("What is your dog's name?\n")
+    dogDogAge = input('Got it. How old is ' + dogName + '?\n')
 
+    # Here, we convert the dog's age from a string (dogIntAge) to a float (dogNewAge)
+    # and back to a string (dogHumanAge) that can be read in the print statement:
+    dogIntAge = float(dogDogAge)
+    dogNewAge = (dogIntAge * 7)
+    dogHumanAge = str(dogNewAge)
 
-
-print("Alright, we\'re going to calculate your dog\'s age in dog years. Let's go!\n")
-# sleep = 1   (How do you write a time delay in a Python program?)
-dogName = input("What is your dog's name?\n")
-dogDogAge = input('Got it. How old is ' + dogName + '?\n')
-
-#dogIntAge = int(dogDogAge)
-dogIntAge = float(dogDogAge)
-dogNewAge = (dogIntAge * 7)
-dogHumanAge = str(dogNewAge)
-
-print('Cool. Your dog, ' + dogName + ', is ' + dogHumanAge + ' in human years.')
+    # Prints the statement, using the dog's name and dog's human age:
+    print('Cool. Your dog, ' + dogName + ', is ' + dogHumanAge + ' in human years.')
